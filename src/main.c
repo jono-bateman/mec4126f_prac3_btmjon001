d872644ee5e9deda06546ae58a6334b17ac505f3
jono-bateman <btmjon001@myuct.ac.za>
2024-03-04 14:30:07 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,38 @@
+// Written by:
+// Date Created:
+// Programmed in: STM32CubeIDE
+// Target: STM32F051 or PC
+// Description:
+
+
+// DEFINES AND INCLUDES
+#define STM32F051
+#include "stm32f0xx.h"
+#include "lcd_stm32f0.h"
+
+
+// GLOBAL VARIABLES
+
+
+// GLOBAL CONSTANTS
+
+
+// FUNCTION DECLARATIONS
+void main(void);
+
+
+// MAIN FUNCTION
+void main(void)
+{
+
+	while(1)
+	{
+
+
+	}
+}
+
+// FUNCTION DEFINITIONS
+
+
+// END OF PROGRAM
