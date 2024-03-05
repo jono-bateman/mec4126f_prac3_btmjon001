eecf870326e3de31523e3a8e36ec301b686143fa
jono-bateman <btmjon001@myuct.ac.za>
2024-03-05 11:10:07 +0200

    Question 4
    declared structs and enums that describe the robot's status

--- src/main.c
+++ src/main.c
@@ -1,17 +1,30 @@
-// Written by:
-// Date Created:
+// Written by: Jono Bateman
+// Date Created: 5 March 2024
 // Programmed in: STM32CubeIDE
 // Target: STM32F051 or PC
-// Description:
+// Description: Prac 3 MEC4126F
 
 
 // DEFINES AND INCLUDES
 #define STM32F051
 #include "stm32f0xx.h"
 #include "lcd_stm32f0.h"
+#define HIGH 1
+#define LOW 0
 
 
 // GLOBAL VARIABLES
+
+enum motion {forward=1, back=-1, right=2, left=3, stop=0};
+
+struct robot_status{
+	enum motion robot_motion;
+	uint8_t ir_left;
+	uint8_t ir_right;
+};
+
+struct robot_status robot_state;
+
 
 
 // GLOBAL CONSTANTS
@@ -25,9 +38,9 @@ void main(void);
 void main(void)
 {
 
+
 	while(1)
 	{
-		delay(2000); // testing if it builds
 
 	}
 }
